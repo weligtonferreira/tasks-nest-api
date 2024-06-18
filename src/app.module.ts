722afad90d3f05dd3cf46262b0eb811e3253d512
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-18 10:37:05 -0300

    :wrench: chore: configure dotenv in the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.env.production.local'
+          : '.env.development.local',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
