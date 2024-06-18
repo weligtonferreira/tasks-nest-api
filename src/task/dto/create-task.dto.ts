98ef34ec9625005e994f9656010bf0ec2c9aa741
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-18 10:51:29 -0300

    :sparkles: feat: add fields to CreateTaskDto

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,1 +1,9 @@
-export class CreateTaskDto {}
+import { TaskStatusEnum } from '../enums/task-status-enum';
+
+export class CreateTaskDto {
+  title: string;
+  descrition: string | null;
+  status: TaskStatusEnum | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
