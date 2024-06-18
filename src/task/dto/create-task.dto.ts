4ed4466da49a7de39e0c09cf07f21cd3e28d16bc
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-18 15:20:44 -0300

    :sparkles: feat: add validation to CreateTaskDto with class-validator library

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,9 +1,34 @@
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 import { TaskStatusEnum } from '../enums/task-status-enum';
 
 export class CreateTaskDto {
+  @MaxLength(255)
+  @MinLength(1)
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsOptional()
   descrition: string | null;
+
+  @IsString()
+  @IsOptional()
   status: TaskStatusEnum | null;
+
+  @IsDateString()
+  @IsOptional()
   createdAt: Date;
+
+  @IsDateString()
+  @IsOptional()
   updatedAt: Date;
 }
