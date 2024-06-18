60f069daf4b909b4b8c4d2c42a0f000ecaf43db6
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-18 10:54:39 -0300

    :sparkles: feat: add attributes to Task entity class

--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,1 +1,37 @@
-export class Task {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+enum TaskStatusEnum {
+  Pending = 'PENDING',
+  Done = 'DONE',
+}
+@Entity('tasks')
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  title: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column({
+    type: 'enum',
+    enum: TaskStatusEnum,
+    default: TaskStatusEnum.Pending,
+    nullable: true,
+  })
+  status: TaskStatusEnum;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
