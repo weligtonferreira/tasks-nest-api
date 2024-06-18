6f66444094b5fe39ff0019d2722b2299065884b4
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-18 16:01:18 -0300

    :sparkles: feat: add HandleAllErrorsExceptionFilter as a global filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
-import { ValidationPipe } from '@nestjs/common';
+import { HandleAllErrorsExceptionFilter } from './expections-filters/handle-all-errors.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new HandleAllErrorsExceptionFilter());
 
   app.useGlobalPipes(
     new ValidationPipe({ errorHttpStatusCode: 422, transform: true }),
