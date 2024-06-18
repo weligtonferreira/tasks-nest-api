81bfe7956a4ed26d519c8c508d096a05a0cc1e8e
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-18 11:04:13 -0300

    :sparkles: feat: add task providers to provide the task repository

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -4,10 +4,11 @@ import { DatabaseModule } from 'src/database/database.module';
 
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
+import { taskProviders } from './task.provider';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [TaskController],
-  providers: [TaskService],
+  providers: [...taskProviders, TaskService],
 })
 export class TaskModule {}
