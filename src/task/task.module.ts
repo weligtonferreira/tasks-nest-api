f407983943fe88f95af50ec4c728d5e60f9c3699
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-18 11:02:14 -0300

    :sparkles: feat: import DatabaseModule into TaskModule

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+
+import { DatabaseModule } from 'src/database/database.module';
+
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [TaskController],
   providers: [TaskService],
 })
