7f329dbe49f24619172ec33ecc5b5d44017670b9
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-18 11:08:35 -0300

    :sparkles: feat: inject the task repository to TaskService constructor

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 
+import { Task } from './entities/task.entity';
+
 @Injectable()
 export class TaskService {
+  constructor(
+    @Inject('TASK_REPOSITORY') private taskRepository: Repository<Task>,
+  ) {}
+
   create(createTaskDto: CreateTaskDto) {
     return 'This action adds a new task';
   }
