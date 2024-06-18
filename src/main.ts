abba8bf2315dc67d1ffb9ff390fd9f1b93b0ba1b
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-18 16:28:21 -0300

    :books: docs: add swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 import { HandleAllErrorsExceptionFilter } from './expections-filters/handle-all-errors.exception-filter';
@@ -15,6 +16,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Tasks API')
+    .setDescription('This is a REST API for task management')
+    .setVersion('1.0')
+    .addTag('tasks')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
