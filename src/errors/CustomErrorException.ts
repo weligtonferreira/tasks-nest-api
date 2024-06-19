977a083d6e6bca08b3e3353d29c0319dc05681ee
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-19 12:30:07 -0300

    :sparkles: feat: add CustomErrorException class

---
+++ src/errors/CustomErrorException.ts
@@ -0,0 +1,8 @@
+export class CustomErrorException extends Error {
+  statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
