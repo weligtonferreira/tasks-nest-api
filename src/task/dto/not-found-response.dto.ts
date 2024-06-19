c5711a36018c6b92591936f3140e0067de790800
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-19 08:40:15 -0300

    :books: docs: add swagger documentation for NotFoundReponseDto

---
+++ src/task/dto/not-found-response.dto.ts
@@ -0,0 +1,18 @@
+import { HttpStatus } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class NotFoundReponseDto {
+  @ApiProperty({
+    nullable: true,
+    example: 'Task not found',
+    description: 'Message of error',
+  })
+  message: string;
+
+  @ApiProperty({
+    nullable: false,
+    example: 404,
+    description: 'Http status code',
+  })
+  statusCode: HttpStatus.NOT_FOUND;
+}
