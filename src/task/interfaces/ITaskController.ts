200bd100730f8a8e357dc874b3c4346b7702ab68
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-19 08:55:26 -0300

    :sparkles: feat: add status to findAll function parameters in ITaskController

--- src/task/interfaces/ITaskController.ts
+++ src/task/interfaces/ITaskController.ts
@@ -5,9 +5,11 @@ import { UpdateTaskDto } from '../dto/update-task.dto';
 
 import { ICreatedTaskResponse } from './ICreatedTaskResponse';
 
+import { TaskStatusEnum } from '../enums/task-status-enum';
+
 export interface ITaskController {
   create: (createTaskDto: CreateTaskDto) => Promise<ICreatedTaskResponse>;
-  findAll: () => Promise<Task[]>;
+  findAll: (status: TaskStatusEnum) => Promise<Task[]>;
   findById: (id: string) => Promise<Task[]>;
   updateById: (id: string, updateTaskDto: UpdateTaskDto) => Promise<void>;
   removeById: (id: string) => Promise<void>;
