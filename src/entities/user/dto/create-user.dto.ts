652ab39fd77f66c2c5ba3be71c9ac3395da2af45
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-19 10:54:45 -0300

    :sparkles: feat: add validation to CreateUserDto

--- src/entities/user/dto/create-user.dto.ts
+++ src/entities/user/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @MaxLength(25)
+  @MinLength(1)
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @MaxLength(100)
+  @MinLength(10)
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @MaxLength(50)
+  @MinLength(8)
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
