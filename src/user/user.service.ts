fdc0d8b4d7e547b58497db022af959b2775ff733
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-19 14:37:43 -0300

    :sparkles: feat: implement IUserService in UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,24 +3,27 @@ import { Repository } from 'typeorm';
 
 import { User } from './entities/user.entity';
 
+import { IUserService } from './interfaces/IUserService';
+import { ICreatedUserResponse } from './interfaces/ICreatedUserResponse';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 import { NotFoundErrorException } from '../errors/NotFoundErrorException';
 
 @Injectable()
-export class UserService {
+export class UserService implements IUserService {
   constructor(
     @Inject('USER_REPOSITORY') private userRepository: Repository<User>,
   ) {}
 
-  async create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto): Promise<ICreatedUserResponse> {
     const newUser = await this.userRepository.save(createUserDto);
 
     return { id: newUser.id };
   }
 
-  async findAll() {
+  async findAll(): Promise<User[]> {
     const users = await this.userRepository.find();
 
     if (users.length === 0) {
@@ -30,7 +33,7 @@ export class UserService {
     return users;
   }
 
-  async findById(id: string) {
+  async findById(id: string): Promise<User[]> {
     const user = await this.userRepository.find({ where: { id } });
 
     if (user.length === 0) {
@@ -40,17 +43,17 @@ export class UserService {
     return user;
   }
 
-  async findByEmail(email: string) {
+  async findByEmail(email: string): Promise<User[]> {
     return this.userRepository.find({ where: { email } });
   }
 
-  async update(id: string, updateUserDto: UpdateUserDto) {
+  async updateById(id: string, updateUserDto: UpdateUserDto): Promise<void> {
     await this.findById(id);
 
     this.userRepository.update(id, updateUserDto);
   }
 
-  async remove(id: string) {
+  async removeById(id: string): Promise<void> {
     await this.findById(id);
 
     this.userRepository.delete(id);
