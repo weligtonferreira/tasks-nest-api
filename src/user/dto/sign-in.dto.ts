dae0b109e64e199239a1ca5ce0efbd15b0e6a463
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-19 14:32:59 -0300

    :books: docs: add swagger documentation for SignInDto

--- src/user/dto/sign-in.dto.ts
+++ src/user/dto/sign-in.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsNotEmpty,
@@ -7,6 +8,13 @@ import {
 } from 'class-validator';
 
 export class SignInDto {
+  @ApiProperty({
+    description: 'E-mail of user',
+    example: 'username@email.com',
+    minLength: 10,
+    maxLength: 100,
+    nullable: false,
+  })
   @MaxLength(100)
   @MinLength(10)
   @IsEmail()
@@ -14,6 +22,13 @@ export class SignInDto {
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    description: 'Password of user',
+    example: 'password',
+    minLength: 8,
+    maxLength: 50,
+    nullable: false,
+  })
   @MaxLength(50)
   @MinLength(8)
   @IsString()
