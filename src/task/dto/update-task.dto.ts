41c868ff6795b863a45ba2abcf9e12438c862487
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-19 08:39:42 -0300

    :books: docs: add swagger documentation for UpdateTaskDto

--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,4 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+import { TaskStatusEnum } from '../enums/task-status-enum';
+
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+  @ApiProperty({
+    description: 'Title of the task',
+    example: 'Make a cake',
+    nullable: true,
+    minLength: 1,
+    maxLength: 255,
+  })
+  title?: string;
+
+  @ApiProperty({
+    description: 'Description of the task',
+    example: 'Make a delicious chocolate cake',
+    nullable: true,
+  })
+  descrition?: string;
+
+  @ApiProperty({
+    description: 'Status of the task',
+    example: 'DONE',
+    nullable: true,
+    enumName: 'TaskStatusEnum',
+    enum: TaskStatusEnum,
+    default: TaskStatusEnum.Pending,
+  })
+  status?: TaskStatusEnum;
+}
