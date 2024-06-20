81d5c043e87ede3856067edd1e5213294ab904e7
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-20 12:53:17 -0300

    :sparkles: feat: add signIn function in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+
+import { SignInDto } from '../user/dto/sign-in.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async signIn(@Body() signInDto: SignInDto) {
+    return await this.authService.signIn(signInDto.email, signInDto.password);
+  }
+}
