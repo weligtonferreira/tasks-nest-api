98c43e360ae3073bbbe3ecfd5003a49e516166ff
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-19 22:17:54 -0300

    :recycle: refactor: change findById function assinature

--- src/user/interfaces/IUserController.ts
+++ src/user/interfaces/IUserController.ts
@@ -1,3 +1,4 @@
+import { FindUserByEmailDto } from './../dto/find-user-by-email.dto';
 import { User } from '../entities/user.entity';
 
 import { CreateUserDto } from '../dto/create-user.dto';
@@ -8,8 +9,8 @@ import { ICreatedUserResponse } from './ICreatedUserResponse';
 export interface IUserController {
   create: (createTaskDto: CreateUserDto) => Promise<ICreatedUserResponse>;
   findAll: () => Promise<User[]>;
-  findById: (id: string) => Promise<User[]>;
-  findByEmail: (email: string) => Promise<User[]>;
+  findById: (id: string) => Promise<User>;
+  findByEmail: (findUserByEmailDto: FindUserByEmailDto) => Promise<User>;
   updateById: (id: string, updateUserDto: UpdateUserDto) => Promise<void>;
   removeById: (id: string) => Promise<void>;
 }
