5df71971821a2ec31f21b9c776d9d91b00d68e01
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-20 14:38:37 -0300

    :books: docs: add swagger documentation for UnauthorizedExceptionResponseDto

--- src/auth/dto/unauthorized-exception-response.dto.ts
+++ src/auth/dto/unauthorized-exception-response.dto.ts
@@ -1,7 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UnauthorizedExceptionResponseDto {
+  @ApiProperty({
+    description: 'Message of error',
+    example: 'Email or password is incorrect',
+  })
   message: string;
 
+  @ApiProperty({
+    description: 'Error name',
+    example: 'Unauthorized',
+  })
   error: string;
 
+  @ApiProperty({
+    description: 'Status code of error',
+    example: 401,
+  })
   statusCode: number;
 }
