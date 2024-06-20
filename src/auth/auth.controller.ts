2a6435e18a4b0d2c120c2ff6c91b5ff6a9be6c65
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-20 13:02:43 -0300

    :sparkles: feat: implement IAuthController interface

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,11 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
-
 import { SignInDto } from '../user/dto/sign-in.dto';
+import { IAuthController } from './interfaces/IAuthController';
 
 @Controller('auth')
-export class AuthController {
+export class AuthController implements IAuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
