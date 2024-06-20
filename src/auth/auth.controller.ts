3a70272a9828c3eac0b8b5955748e9265bc1870a
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-20 14:40:08 -0300

    :books: docs: add swagger documentation for AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,12 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiParam, ApiResponse } from '@nestjs/swagger';
 
 import { AuthService } from './auth.service';
+
 import { SignInDto } from '../user/dto/sign-in.dto';
 import { IAuthController } from './interfaces/IAuthController';
+import { JwtTokenReponseDto } from './dto/jwt-token-response.dto';
+import { UnauthorizedExceptionResponseDto } from './dto/unauthorized-exception-response.dto';
 
 @Controller('auth')
 export class AuthController implements IAuthController {
@@ -10,6 +14,24 @@ export class AuthController implements IAuthController {
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
+  @ApiParam({
+    name: 'email',
+    description: 'E-mail of user',
+  })
+  @ApiParam({
+    name: 'password',
+    description: 'Password of user',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Return the jwt token',
+    type: [JwtTokenReponseDto],
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Email or password is incorrect',
+    type: [UnauthorizedExceptionResponseDto],
+  })
   async signIn(@Body() signInDto: SignInDto) {
     return await this.authService.signIn(signInDto.email, signInDto.password);
   }
