4996f99153c26127a79cf7339a7e261075ee504e
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-19 22:45:39 -0300

    :recycle: refactor: remove the global filters

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-import { HandleValidationErrorsExceptionFilter } from './exceptions-filters/handle-validation-errors.exception-filter';
-import { HandleCustomErrorsExceptionFilter } from './exceptions-filters/handle-custom-errors.exception-filter';
-
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalFilters(
-    new HandleValidationErrorsExceptionFilter(),
-    new HandleCustomErrorsExceptionFilter(),
-  );
 
   app.useGlobalPipes(
     new ValidationPipe({
