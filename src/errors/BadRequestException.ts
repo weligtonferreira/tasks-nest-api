9cba4aa6e3b670b3d631fc84b84b4d94ee892e38
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-20 09:41:51 -0300

    :recycle: refactor: change name of BadRequestException

--- src/errors/BadRequestException.ts
+++ src/errors/BadRequestException.ts
@@ -2,7 +2,7 @@ import { HttpStatus } from '@nestjs/common';
 
 import { CustomErrorException } from './CustomErrorException';
 
-export class BadRequestErrorException extends CustomErrorException {
+export class BadRequestException extends CustomErrorException {
   constructor(message: string) {
     super(message, HttpStatus.NOT_FOUND);
   }
