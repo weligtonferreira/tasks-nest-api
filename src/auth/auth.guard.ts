87aebe10c3d335e70188554dddea84fb3939686c
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-20 15:30:50 -0300

    :sparkles: feat: add validation to determine if the route is public

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -9,6 +9,8 @@ import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 
+import { IS_PUBLIC_KEY } from './public.decorator';
+
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
@@ -18,6 +20,15 @@ export class AuthGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest();
 
     const token = this.extractTokenFromHeader(request);
