f6ea48182fdab6a395a6747df28284a949dae8b5
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-20 10:38:59 -0300

    :sparkles: feat: add isValidStatus validation function

--- src/task/enums/task-status-enum.ts
+++ src/task/enums/task-status-enum.ts
@@ -2,3 +2,7 @@ export enum TaskStatusEnum {
   Pending = 'PENDING',
   Done = 'DONE',
 }
+
+export function isValidStatus(status: string): boolean {
+  return Object.values(TaskStatusEnum).includes(status as TaskStatusEnum);
+}
