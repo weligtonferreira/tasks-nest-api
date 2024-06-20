ea94946bcba326e2d17ff4e02568f666c775a1b3
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-20 12:56:57 -0300

    :sparkles: feat: add signIn function in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+import { UserService } from '../user/user.service';
+import { JwtTokenReponseDto } from './dto/jwt-token-response.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(email: string, password: string): Promise<JwtTokenReponseDto> {
+    const user = await this.userService.findByEmailForAuthentication(email);
+
+    if (!user) {
+      throw new UnauthorizedException('Email or password is incorrect');
+    }
+
+    const isCorrectPassword = await bcrypt.compare(password, user.password);
+
+    if (!isCorrectPassword) {
+      throw new UnauthorizedException('Email or password is incorrect');
+    }
+
+    const jwtConfig = {
+      payload: {
+        sub: user.id,
+        email: user.email,
+      },
+      options: {
+        expiresIn: '2d',
+        secret: 'SECRET',
+      },
+    };
+
+    const { payload, options } = jwtConfig;
+
+    const access_token = await this.jwtService.signAsync(payload, options);
+
+    return { access_token };
+  }
+}
