958623c4da9fe270e5183894c39501785b411a35
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-21 09:22:54 -0300

    :books: docs: add swagger api tag for auth routes in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { ApiParam, ApiResponse } from '@nestjs/swagger';
+import { ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { AuthService } from './auth.service';
 
@@ -9,6 +9,7 @@ import { JwtTokenReponseDto } from './dto/jwt-token-response.dto';
 import { UnauthorizedExceptionResponseDto } from './dto/unauthorized-exception-response.dto';
 import { Public } from './public.decorator';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController implements IAuthController {
   constructor(private authService: AuthService) {}
